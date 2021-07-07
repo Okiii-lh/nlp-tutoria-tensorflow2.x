31583519ef2b31c6611e6d3bed6df5d98c1ffde2
liuhe_tech <13132515202@163.com>
2021-07-07 13:19:05 +0800

    implement Word2Vec

--- 1-2.Word2Vec(Skipgram)/Word2Vec-Skipgram(Softmax).py
+++ 1-2.Word2Vec(Skipgram)/Word2Vec-Skipgram(Softmax).py
@@ -9,6 +9,8 @@ from tensorflow import keras
 from tensorflow.keras import Model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing import sequence
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Word2Vec(Model):
@@ -20,11 +22,16 @@ class Word2Vec(Model):
         :param vocab_size:
         :param max_len:
         """
+        super(Word2Vec, self).__init__()
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
 
-        self.hidden_layer = keras.layers.Dense(embedding_dim, use_bias=False)
-        self.output_layer = keras.layers.Dense(vocab_size, use_bias=False)
+        self.hidden_layer = keras.layers.Dense(self.embedding_dim,
+                                               use_bias=False, name='dense')
+        self.output_layer = keras.layers.Dense(self.vocab_size,
+                                               use_bias=False,
+                                               activation='softmax',
+                                               name='output')
 
     def call(self, inputs):
         hidden = self.hidden_layer(inputs)
@@ -33,13 +40,24 @@ class Word2Vec(Model):
         return output
 
 
-def data_generator(batch_size, skip_gram):
+def data_generator(skip_gram, vocab_size):
     """
-    generate data
-    :param batch_size: batch size
+    generate train data
     :param skip_gram: corpus
+    :param vocab_size: length of word dict
     :return:
     """
+    train_x =[]
+    train_y = []
+    for word in skip_gram:
+        xi = np.zeros(vocab_size).tolist()
+        xi[word[0]] = 1
+        train_x.append(xi)
+        yi = np.zeros(vocab_size).tolist()
+        yi[word[1]] = 1
+        train_y.append(yi)
+
+    return train_x, train_y
 
 
 if __name__ == '__main__':
@@ -52,7 +70,6 @@ if __name__ == '__main__':
     word_dic = {w: i for i, w in enumerate(vocab)}
     vocab_size = len(vocab)
 
-    print(word_dic)
     skip_grams = []
     for sentence in sentences:
         words = sentence.split()
@@ -62,4 +79,15 @@ if __name__ == '__main__':
             for context in context_word:
                 skip_grams.append([head_word, context])
 
-    print(skip_grams)
+    x, y = data_generator(skip_grams, vocab_size)
+    model = Word2Vec(vocab_size, 10)
+
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=keras.losses.CategoricalCrossentropy(), metrics=[
+            'accuracy'])
+    model.fit(x, y, epochs=1000, batch_size=5)
+
+    for i, label in enumerate(vocab):
+        x = model.get_layer('dense').get_weights()[0][i]
+
+        print(label, ": ", x)
